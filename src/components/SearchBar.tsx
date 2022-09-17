397e4370fde1fa1a1a61bffa7792d6d4b83c0f8e
Jaehun Kim <crisplaver@gmail.com>
2022-09-17 16:21:35 +0900

    emotion/react 추가

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,12 @@
+const SearchBar = () => {
+
+
+    return (
+        <div>
+            <div css={{ color: 'red' }}>Test</div>
+            <input />
+        </div>
+    )
+}
+
+export default SearchBar
