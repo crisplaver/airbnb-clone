126fecee64559676f18c4fb0430277a52c8b764c
Jaehun Kim <crisplaver@gmail.com>
2022-09-20 16:54:42 +0900

    숙소 목록 스타일 수정

--- src/components/RoomList.tsx
+++ src/components/RoomList.tsx
@@ -13,8 +13,22 @@ const RoomList = ({
         <div css={containerCss}>
             <div css={{
                 display: 'grid',
-                gridTemplateColumns: 'repeat(2, 1fr)',
-                gap: '40px 24px'
+                gridTemplateColumns: 'repeat(var(--breakpoint-grid_columns, 1), minmax(0, 1fr))',
+                gap: '40px 24px',
+                paddingLeft: 'var(--padding, 24px)', 
+                paddingRight: 'var(--padding, 24px)', 
+                marginTop: 16,
+                marginBottom: 40,
+                '@media (min-width: 550px)': {
+                    '--breakpoint-grid_columns': 2,
+                    '--padding': '40px'
+                },
+                '@media (min-width: 950px)': {
+                    '--breakpoint-grid_columns': 3,
+                },
+                '@media (min-width: 1128px)': {
+                    '--breakpoint-grid_columns': 4
+                },
             }}>
                 {items.map(item => (
                     <RoomListItem
@@ -52,19 +66,29 @@ const RoomListItem = ({
 }) => {
     return (
         <div>
-            <img src={pictures[0]} />
-            <div css={{ display: 'flex', flexDirection: 'row' }}>
-                <div>{title}</div>
+            <div css={{ borderRadius: 12, overflow: 'clip', marginBottom: 12, width: '100%', aspectRatio: '20 / 19' }}>
+                <img src={pictures[0]} css={{ objectFit: 'cover', width: '100%', height: '100%' }} />
+            </div>
+            <div css={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <div
+                    css={{
+                        fontSize: 15,
+                        fontWeight: 600,
+                        whiteSpace: 'nowrap',
+                        textOverflow: 'ellipsis',
+                        overflow: 'hidden',
+                        marginRight: 8
+                    }}>{title}</div>
                 <div css={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
                     <StarIcon />
-                    <div>{avgRating}</div>
+                    <div css={{ fontSize: 15, marginLeft: 4 }}>{avgRating || 'NEW'}</div>
                 </div>
             </div>
-            <div>{location}</div>
-            <div>{period}</div>
-            <div css={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                <div>{price}</div>
-                <div>{qualifier}</div>
+            <div css={{ fontSize: 15, marginTop: 2 }}>{location}</div>
+            <div css={{ fontSize: 15, marginTop: 2 }}>{period}</div>
+            <div css={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: 8 }}>
+                <div css={{ fontSize: 15, fontWeight: 600 }}>{price}</div>
+                <div css={{ fontSize: 15 }}>&nbsp;{qualifier}</div>
             </div>
         </div>
     )
