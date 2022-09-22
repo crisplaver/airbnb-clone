c9b48e504d1029a92676ae8a93ccf47aa51e9d6b
Jaehun Kim <crisplaver@gmail.com>
2022-09-22 13:58:50 +0900

    상단 탭 hover 애니메이션 추가

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -20,7 +20,6 @@ const Tabs = ({
                     key={index}
                     title={item.title}
                     imageUrl={item.imageUrl}
-                    containerCss={{ margin: '14px 12px' }}
                     selected={index === selectedIndex}
                     onClick={() => setSelectedIndex(index)}
                 />
@@ -32,62 +31,74 @@ const Tabs = ({
 const Tab = ({
     title,
     imageUrl,
-    containerCss,
     selected,
     onClick
 }: {
     title: string,
     imageUrl: string,
-    containerCss?: Interpolation<Theme>,
     selected: boolean,
     onClick: () => void
 }) => {
     return (
-        <div css={containerCss} >
-            <button css={{ backgroundColor: 'transparent', border: 0 }} onClick={onClick}>
-                <div
-                    css={{
-                        width: 'max-content',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        filter: selected ? 'contrast(1)' : 'contrast(calc(13 / 44))',
-                        '--display': selected ? 'block' : 'none',
-                        '--bar-color': 'black',
-                        gap: '4px',
-                        ":hover": {
+        <button
+            css={{
+                backgroundColor: 'transparent',
+                border: 0
+            }}
+            onClick={onClick}>
+            <div
+                css={{
+                    width: 'max-content',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    '--opacity': selected ? 1 : 0,
+                    '--bar-color': selected ? 'black' : '#DDDDDD',
+                    padding: '14px 12px',
+                    gap: '4px',
+                    ":hover": {
+                        '--opacity': 1,
+                        img: {
                             filter: 'contrast(1)',
-                            '--display': 'block',
-                            '--bar-color': selected ? 'black' : '#DDDDDD'
+                        },
+                        span: {
+                            filter: 'contrast(1)',
                         }
-                    }}>
-                    <img
-                        src={imageUrl}
-                        css={{ width: 24, height: 24 }}
-                    />
-                    <div css={{
-                        position: 'relative',
-                        "&:after": {
-                            content: '""',
-                            backgroundColor: 'var(--bar-color)',
-                            height: 2,
-                            top: 'calc(100% + 13px)',
-                            insetInline: 0,
-                            position: 'absolute',
-                            display: 'var(--display)'
-                        }
-                    }}>
-                        <span
-                            css={{
-                                fontWeight: 600,
-                                fontSize: 12,
-                            }}>
-                            {title}
-                        </span>
-                    </div>
+                    }
+                }}>
+                <img
+                    src={imageUrl}
+                    css={{
+                        width: 24,
+                        height: 24,
+                        filter: selected ? 'contrast(1)' : 'contrast(calc(13 / 44))',
+                        transition: 'all 0.2s',
+                    }}
+                />
+                <div css={{
+                    position: 'relative',
+                    "&:after": {
+                        content: '""',
+                        backgroundColor: 'var(--bar-color)',
+                        height: 2,
+                        top: 'calc(100% + 13px)',
+                        insetInline: 0,
+                        position: 'absolute',
+                        opacity: 'var(--opacity)'
+                    }
+                }}>
+                    <span
+                        css={{
+                            fontWeight: 600,
+                            fontSize: 12,
+                            filter: selected ? 'contrast(1)' : 'contrast(calc(13 / 44))',
+                            transition: 'all 0.2s',
+                        }}>
+                        {title}
+                    </span>
                 </div>
-            </button >
-        </div>
+            </div>
+        </button >
     )
 }
 
